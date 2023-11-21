e891d769dbe8be2651d54b9ae23eef32e992d208
Arbab Tahir <arbabtahir2244@gmail.com>
2023-11-22 00:42:09 +0500

    All issues resolved!

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,9 @@
 <template>
-    <header id="header" class="border-bottom-0" data-sticky-shrink="false">
-        <div id="header-wrap" class="shadow">
-            <div class="container">
-                <div class="header-row">
-                    <!-- Logo
+        <nav class="navbar navbar-expand-lg shadow">
+    <div class="container">
+     <!-- Logo
 						============================================= -->
-                    <div id="logo">
+                        <div id="logo">
                         <NuxtLink to="/">
                             <img
                                 class="logo-default"
@@ -20,61 +18,64 @@
                         </NuxtLink>
                     </div>
                     <!-- #logo end -->
+      <button
+      class="navbar-toggler custom-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-                    <div class="primary-menu-trigger">
-                        <button
-                            class="cnvs-hamburger"
-                            type="button"
-                            title="Open Mobile Menu"
-                        >
-                            <span class="cnvs-hamburger-box"
-                                ><span class="cnvs-hamburger-inner"></span
-                            ></span>
-                        </button>
-                    </div>
-
-                    <!-- Primary Navigation
-						============================================= -->
-                    <nav class="primary-menu">
-                        <ul class="menu-container">
-                            <li
-                                class="menu-item"
-                                :class="{ current: activeRoute === '/services' }"
+      <div
+      class="collapse navbar-collapse justify-content-end"
+      id="navbarNav">
+        <ul class="navbar-nav mt-3">
+                                <li
+                                class="nav-item"
                             >
-                                <NuxtLink class="menu-link" to="/services"
-                                    ><div>Services</div></NuxtLink
+                                <NuxtLink
+                                class="nav-link" to="/services"
+                                :class="{ active: activeRoute === '/services' }"
                                 >
+                                    <div @click="closeNavbar">Services</div>
+                                    </NuxtLink>
                             </li>
                             <li
-                                class="menu-item"
-                                :class="{ current: activeRoute === '/company' }"
-                            >
-                                <NuxtLink class="menu-link" to="/company"
-                                    ><div>Company</div></NuxtLink
+                                class="nav-item">
+                                <NuxtLink
+                                class="nav-link" to="/company"
+                                :class="{ active: activeRoute === '/company' }"
                                 >
+                                <div @click="closeNavbar">Company</div>
+                                </NuxtLink>
                             </li>
                             <li
-                                class="menu-item"
-                                :class="{ current: activeRoute === '/work' }"
-                            >
-                                <NuxtLink class="menu-link" to="/work"
-                                    ><div>Our Work</div></NuxtLink
+                                class="nav-item">
+                                <NuxtLink
+                                class="nav-link" to="/work"
+                                :class="{ active: activeRoute === '/work' }"
                                 >
+                                <div @click="closeNavbar">Our Work</div>
+                                </NuxtLink>
                             </li>
                             <li
-                                class="menu-item"
-                                :class="{ current: activeRoute === '/contact' }"
-                            >
-                                <NuxtLink class="menu-link" to="/contact"
-                                    ><div>Contact</div></NuxtLink
+                                class="nav-item">
+                                <NuxtLink
+                                class="nav-link" to="/contact"
+                                :class="{ active: activeRoute === '/contact' }"
                                 >
+                                <div @click="closeNavbar">Contact</div>
+                                </NuxtLink>
                             </li>
-                            <li>
+                            <div class="s-icon d-flex">
+                                <li>
                           <div>
                             <a
                                 target="_blank"
                                 href="//facebook.com/noblecospace"
-                                class="social-icon h-bg-facebook"
+                                class="mt-2 social-icon h-bg-facebook"
                             >
                                 <i class="fa-brands fa-facebook-f"></i>
                                 <i class="fa-brands fa-facebook-f"></i>
@@ -87,7 +88,7 @@
                             <a
                                 target="_blank"
                                 href="//twitter.com/noblecospace"
-                                class="social-icon h-bg-twitter"
+                                class="mt-2 social-icon h-bg-twitter"
                             >
                                 <i class="fa-brands fa-twitter"></i>
                                 <i class="fa-brands fa-twitter"></i>
@@ -100,21 +101,19 @@
                             <a
                                 target="_blank"
                                 href="//instagram.com/noblecospace"
-                                class="social-icon h-bg-instagram"
+                                class="mt-2 social-icon h-bg-instagram"
                             >
                                 <i class="fa-brands fa-instagram"></i>
                                 <i class="fa-brands fa-instagram"></i>
                             </a>
                         </div>
                             </li>
-                        </ul>
-                    </nav>
-                    <!-- #primary-menu end -->
-                </div>
-            </div>
-        </div>
-        <div class="header-wrap-clone"></div>
-    </header>
+                            </div>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -125,10 +124,41 @@ export default {
             return this.$route.path;
         },
     },
+
+    methods: {
+        closeNavbar() {
+      this.$nextTick(() => {
+        $('#navbarNav').collapse('hide');
+      });
+    },
+  },
+
 };
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 #logo img {
     height: 80px !important;
+}
+.navbar-nav .nav-item .nav-link {
+    margin-top: .5rem;
+}
+.navbar-nav .nav-item .nav-link:hover,
+.navbar-nav .nav-item .nav-link.active {
+  color: #28ACE2;
+}
+.custom-toggler {
+  outline: none !important;
+  border: none !important;
+  box-shadow: none !important;
+}
+
+@media (max-width: 768px){
+    .s-icon {
+  display: flex;
+  margin-top: 2rem;
+  border-top: 1px solid #28ACE2;
+  border-bottom: 1px solid #28ACE2;
+  justify-content: center;
+}
 }
 </style>
